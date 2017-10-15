47ca012120b9d6707d22e5be26be7539726ec22f
Andy Fang <andy@ramblin.cc.gt.atl.ga.us>
2017-10-15 13:57:34 -0400

    SEPlib is building

--- Born/build.sh
+++ Born/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 module load compiler/xlc
 module load tbb
+module load cmake cuda
 export CMAKE_PREFIX_PATH=$TBBROOT:/usr/local/lib:$CMAKE_PREFIX_PATH
 
 sudo rm -rf build
 mkdir build
 cd build
-cmake -DgenericIO_DIR=/opt/scc/born/genericIO/lib -DCMAKE_INSTALL_PREFIX=/opt/scc/born/Born2 -Dhypercube_DIR=/opt/scc/born/hypercube/lib ..
+cmake -DSEPlib_DIR=/opt/scc/born/SEPlib/lib -DgenericIO_DIR=/opt/scc/born/genericIO/lib -DCMAKE_INSTALL_PREFIX=/opt/scc/born/Born2 -Dhypercube_DIR=/opt/scc/born/hypercube/lib ..
 make -j
 sudo make install
