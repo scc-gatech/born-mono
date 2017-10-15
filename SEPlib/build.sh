d2494b30981ebcd9e3b514915b1af103f2696578
Andy Fang <andy@ramblin.cc.gt.atl.ga.us>
2017-10-15 13:48:26 -0400

    cleanup

--- SEPlib/build.sh
+++ SEPlib/build.sh
@@ -2,7 +2,7 @@
 module load compiler/xlc
 module load tbb
 export CMAKE_PREFIX_PATH=$TBBROOT:/usr/local/lib:$CMAKE_PREFIX_PATH
-
+sudo mkdir -p /opt/scc/born/SEPlib/include
 sudo rm -rf build
 mkdir build
 cd build
